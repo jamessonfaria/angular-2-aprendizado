1f1fe8bb34061d507a046312870855d8a491b007
Jamesson Faria <jamessonjr@gmail.com>
2017-08-27 20:04:54 -0300

    update project

---
+++ client/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FotoModule } from "./foto/foto.module";
+import { HttpModule } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { PainelModule } from "./painel/painel.module";
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { ListagemComponent } from "./listagem/listagem.component";
+import { routing } from "./app.routes";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+    imports: [ BrowserModule, FotoModule, HttpModule, PainelModule, routing, FormsModule ], 
+    declarations: [  AppComponent, CadastroComponent, ListagemComponent ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule {}
---
+++ client/app/cadastro/cadastro.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+import { FotoComponent } from "../foto/foto.component";
+import { Http, Headers } from "@angular/http";
+
+@Component({
+    moduleId: module.id,
+    selector: 'cadastro',
+    templateUrl: './cadastro.component.html'
+})
+
+export class CadastroComponent {
+    foto: FotoComponent = new FotoComponent();
+    http: Http;
+
+    constructor(http: Http){
+        this.http = http;
+    }
+
+    cadastrar(event) {
+        event.preventDefault();
+        console.log(this.foto);
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        this.http
+            .post('v1/fotos', JSON.stringify(this.foto), { headers: headers})
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                console.log("foto salva com sucesso")
+            }, erro => console.log(erro));
+
+    }
+
+}
