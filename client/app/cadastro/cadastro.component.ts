8e5d28e4d1ef7459f20b50217186f181d5416f60
Jamesson Faria <jamessonjr@gmail.com>
2017-08-29 13:17:08 -0300

    update project

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { FotoComponent } from "../foto/foto.component";
-import { Http, Headers } from "@angular/http";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FotoService } from "../foto/foto.service";
 
 @Component({
     moduleId: module.id,
@@ -11,11 +11,12 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 export class CadastroComponent {
     foto: FotoComponent = new FotoComponent();
-    http: Http;
     meuForm: FormGroup;
+    service: FotoService;
 
-    constructor(http: Http, fb: FormBuilder){
-        this.http = http;
+    constructor(service: FotoService, fb: FormBuilder){
+        this.service = service;
+   
         this.meuForm = fb.group({
             titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
             url: ['', Validators.required],
@@ -26,13 +27,11 @@ export class CadastroComponent {
     cadastrar(event) {
         event.preventDefault();
         console.log(this.foto);
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        this.http
-            .post('v1/fotos', JSON.stringify(this.foto), { headers: headers})
+        this.service
+            .cadastra(this.foto)
             .subscribe(() => {
-                this.foto = new FotoComponent();
                 console.log("foto salva com sucesso")
+                this.foto = new FotoComponent();
             }, erro => console.log(erro));
 
     }
--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-import { Http } from "@angular/http";
+import { FotoService } from "../foto/foto.service";
+import { FotoComponent } from "../foto/foto.component";
 
 @Component({
     moduleId: module.id,
@@ -8,14 +9,13 @@ import { Http } from "@angular/http";
 })
 
 export class ListagemComponent {
-    fotos: Object[] = []; 
+    fotos: FotoComponent[] = []; 
 
-    constructor(http: Http){
-        http
-        .get('v1/fotos')
-        .map(res => res.json())
-        .subscribe(fotos => {
-            this.fotos = fotos;
-        }, erro => console.log(erro));
+    constructor(service: FotoService){
+        service
+            .lista()
+            .subscribe(fotos => {
+                this.fotos = fotos;
+            }, erro => console.log(erro));
     }
 }
